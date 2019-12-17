7f4dd53ac310a6b7b8bba182eb8548f4c8f25a79
Pedro Ribeiro <pedro.alex.ribeiro@hotmail.com>
2019-12-17 19:04:14 +0000

    linear regression

--- src/linearRegression.R
+++ src/linearRegression.R
@@ -22,7 +22,7 @@ D = read.table("data.out",header=TRUE, fill=TRUE)
 index = 1 + val + errorProb
 for(i in seq(1,4)){
     print(strings[i])
-    lr.out = lm(1/D[index:(index+tests), "SizeN"] ~ D[index:(index+tests), "Longest"])
+    lr.out = lm(1/D[index:(index+tests), "Longest"] ~ D[index:(index+tests), "SizeN"])
     summ <- summary(lr.out)
 
     print("------------------------PRINTING RESULTS---------------------------")
@@ -46,32 +46,49 @@ for(i in seq(1,4)){
 }
 
 
-
+# "quicksort"
 print(strings[1])
 index = 1 + val + errorProb
-lr.out = lm(1/D[index:(index+tests), "SizeN"] ~ D[index:(index+tests), "nSteps"])
-summary(lr.out)
+lr.out = lm(D[index:(index+tests), "nSteps"] ~ D[index:(index+tests), "SizeN"])
+summ <- summary(lr.out)
+print(summary(lr.out))
+a = summ$coefficients[1]
+b = summ$coefficients[2]
+curve(a + b*x,min_number_elements,max_number_elements)
+points(D[index:(index+tests), "SizeN"], D[index:(index+tests), "nSteps"], col="red")
 
+# "bubblesort"
 print(strings[2])
 index = index + alg
-lr.out = lm(1/D[index:(index+tests), "SizeN"] ~ D[index:(index+tests), "nSteps"])
-summary(lr.out)
+lr.out = lm(D[index:(index+tests), "nSteps"] ~ D[index:(index+tests), "SizeN"])
+summ <- summary(lr.out)
+print(summary(lr.out))
+a = summ$coefficients[1]
+b = summ$coefficients[2]
+curve(a + b*x,min_number_elements,max_number_elements) 
+points(D[index:(index+tests), "SizeN"], D[index:(index+tests), "nSteps"], col="red")
 
+# "mergesort"
 print(strings[3])
 index = index + alg
-lr.out = lm(1/D[index:(index+tests), "SizeN"] ~ D[index:(index+tests), "nSteps"])
-summary(lr.out)
+lr.out = lm(D[index:(index+tests), "nSteps"] ~ D[index:(index+tests), "SizeN"])
+summ <- summary(lr.out)
+print(summary(lr.out))
+a = summ$coefficients[1]
+b = summ$coefficients[2]
+curve(a + b*x,min_number_elements,max_number_elements) 
+points(D[index:(index+tests), "SizeN"], D[index:(index+tests), "nSteps"], col="red")
 
+# "insertionsort"
 print(strings[4])
 index = index + alg
-lr.out = lm(1/D[index:(index+tests), "SizeN"] ~ D[index:(index+tests), "nSteps"])
-summary(lr.out)
-
-
-
-
-
-
+lr.out = lm(D[index:(index+tests), "nSteps"] ~ D[index:(index+tests), "SizeN"])
+summ <- summary(lr.out)
+print(summary(lr.out))
+a = summ$coefficients[1]
+b = summ$coefficients[2]
+curve(a + b*x,min_number_elements,max_number_elements) 
+points(D[index:(index+tests), "SizeN"], D[index:(index+tests), "nSteps"], col="red")
 
 # Quicksort
 # r^2 -> 0.9828 | r -> 0.9819
